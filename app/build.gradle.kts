ce13743564a2dac5e696e1db11a7610813a520ad
maksimus <max.expertdev@gmail.com>
2025-10-01 19:03:21 +0600

    sonarqube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("application")
     id("se.patrikerdes.use-latest-versions") version "0.2.19"
     id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "6.3.1.5724"
 }
 
 group = "hexlet.code"
@@ -19,6 +20,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "mtvru_java-project-61")
+        property("sonar.organization", "mtvru")
+    }
 }
 
 tasks.test {
